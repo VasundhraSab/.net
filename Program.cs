1b2542281c5a9b9688bfb5d88f0afd9de72c122e
VasundhraSab <43847989+VasundhraSab@users.noreply.github.com>
2018-10-04 18:08:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventdemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer(100000);
+
+            Console.Write("Please enter the amount to withdraw");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            c.withdraw(amount);
+
+            //Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
